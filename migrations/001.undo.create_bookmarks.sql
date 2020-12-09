cdbdf3c285edfc637cb60174c67b59c2c81d5c15
yefenny <yefennychireno@Yefennys-MacBook-Pro.local>
2020-12-09 06:19:35 -0500

    create and seed database

---
+++ migrations/001.undo.create_bookmarks.sql
@@ -0,0 +1,4 @@
+-- ALTER table bookmarks drop column if exists rating;
+-- DROP TYPE if exists rating_range;
+
+DROP TABLE IF EXISTS bookmarks;
---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,8 @@
+INSERT  INTO bookmarks (title, url, description,rating) values ('Google','https://www.google.com/','A search engine', 3),
+('Google 2','https://www.google.com/','A search engine', 2),
+('Google 3','https://www.google.com/','A search engine', 4),
+('Yahoo','https://www.yahoo.com/','Another search engine', 5),
+('Facebook','https://www.facebook.com/','A social media', 3),
+('Youtube','https://www.youtube.com/','Where I can watch videos', 2),
+('Github','https://www.github.com/','Where all these will be stored', 4),
+('Instagram','https://www.instagram.com/','Social media that makes me lose a lot of time', 3);
